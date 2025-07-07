25f28cdd1bbd234b6cae296208ce7c41ed611138
Roman <randey.row@gmail.com>
2025-07-07 15:24:12 +0200

    updated cmake comments

--- src/gui.h
+++ src/gui.h
@@ -18,14 +18,14 @@
 #include <cstdio>
 
 // ---------------------------------------------------------------------------
-static void glfw_error_callback(int error, const char* desc)
+static void glfw_error_callback(int error, const char *desc)
 {
     fprintf(stderr, "GLFW Error %d: %s\n", error, desc);
 }
 
 // ---------------------------------------------------------------------------
 // Utility: derive a sibling file path with a new extension
-static std::string change_extension(const char* path, const char* new_ext)
+static std::string change_extension(const char *path, const char *new_ext)
 {
     std::filesystem::path p(path);
     p.replace_extension(new_ext);
@@ -37,44 +37,47 @@ inline int run_gui()
 {
     // Init GLFW + GL context
     glfwSetErrorCallback(glfw_error_callback);
-    if (!glfwInit()) return 1;
+    if (!glfwInit())
+        return 1;
 
-    const char* glsl_version = "#version 130";
-    GLFWwindow* window = glfwCreateWindow(960, 600, "Image Codec Demo", nullptr, nullptr);
-    if (!window) return 1;
+    const char *glsl_version = "#version 130";
+    GLFWwindow *window = glfwCreateWindow(960, 600, "Image Codec Demo", nullptr, nullptr);
+    if (!window)
+        return 1;
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
     // Init ImGui
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
     // ------ UI state ----------
     // HEIC
-    char heic_in[512]  = "";
+    char heic_in[512] = "";
     char heic_out[512] = "";
-    int  heic_quality  = 90;
-    bool heic_encode   = true;
-    bool heic_done_ok  = false;
-    bool heic_show_msg = false;   // whether to show the green banner
+    int heic_quality = 90;
+    bool heic_encode = true;
+    bool heic_done_ok = false;
+    bool heic_show_msg = false; // whether to show the green banner
 
     // JPEG
-    char jpg_in[512]   = "";
-    char jpg_out[512]  = "";
-    int  jpg_quality   = 90;
-    bool jpg_encode    = true;
-    bool jpg_done_ok   = false;
-    bool jpg_show_msg  = false;
+    char jpg_in[512] = "";
+    char jpg_out[512] = "";
+    int jpg_quality = 90;
+    bool jpg_encode = true;
+    bool jpg_done_ok = false;
+    bool jpg_show_msg = false;
 
     // PSNR sweep
     char psnr_img[512] = "";
     char psnr_csv[512] = "";
-    bool psnr_is_jpeg  = true;
-    bool psnr_done_ok  = false;
+    bool psnr_is_jpeg = true;
+    bool psnr_done_ok = false;
     bool keep_tmp_files = false;
     bool psnr_show_msg = false;
 
@@ -90,13 +93,14 @@ inline int run_gui()
         ImGui::NewFrame();
 
         // ---------------- HEIC WINDOW ----------------
-        if (first_frame) {
+        if (first_frame)
+        {
             ImGui::SetNextWindowPos(ImVec2(10, 10));
             ImGui::SetNextWindowSize(ImVec2(300, 240));
         }
         ImGui::Begin("HEIC Compressor / Decompressor");
 
-        ImGui::InputText("Input Path",  heic_in,  IM_ARRAYSIZE(heic_in));
+        ImGui::InputText("Input Path", heic_in, IM_ARRAYSIZE(heic_in));
         ImGui::InputText("Output Path", heic_out, IM_ARRAYSIZE(heic_out));
 
         ImGui::Checkbox("Encode (uncheck = Decode)", &heic_encode);
@@ -105,8 +109,8 @@ inline int run_gui()
 
         if (ImGui::Button(heic_encode ? "Encode" : "Decode"))
         {
-            heic_show_msg = false;           
-            heic_done_ok  = false;
+            heic_show_msg = false;
+            heic_done_ok = false;
             if (std::strlen(heic_in) != 0)
             {
                 // If output not provided, derive it
@@ -115,18 +119,21 @@ inline int run_gui()
                     std::string def = change_extension(heic_in, heic_encode ? ".heic" : ".png");
                     std::strncpy(heic_out, def.c_str(), sizeof(heic_out));
                 }
-                try {
+                try
+                {
                     if (heic_encode)
                     {
-                        HeicEncoder enc(heic_in, heic_out);  // updated constructor with output_path
+                        HeicEncoder enc(heic_in, heic_out); // updated constructor with output_path
                         heic_done_ok = enc.encode(heic_quality);
                     }
                     else
                     {
-                        HeicDecoder dec(heic_in, heic_out);  // updated constructor with output_path
+                        HeicDecoder dec(heic_in, heic_out); // updated constructor with output_path
                         heic_done_ok = dec.decode();
                     }
-                } catch (...) {
+                }
+                catch (...)
+                {
                     heic_done_ok = false;
                 }
             }
@@ -134,17 +141,18 @@ inline int run_gui()
         }
 
         if (heic_show_msg)
-            ImGui::TextColored(ImVec4(0,1,0,1), "Success! Saved to %s", heic_out);
+            ImGui::TextColored(ImVec4(0, 1, 0, 1), "Success! Saved to %s", heic_out);
         ImGui::End();
 
         // ---------------- JPEG WINDOW ----------------
-        if (first_frame) {
+        if (first_frame)
+        {
             ImGui::SetNextWindowPos(ImVec2(330, 10));
             ImGui::SetNextWindowSize(ImVec2(300, 240));
         }
         ImGui::Begin("JPEG Compressor / Decompressor");
 
-        ImGui::InputText("Input Path",  jpg_in,  IM_ARRAYSIZE(jpg_in));
+        ImGui::InputText("Input Path", jpg_in, IM_ARRAYSIZE(jpg_in));
         ImGui::InputText("Output Path", jpg_out, IM_ARRAYSIZE(jpg_out));
 
         ImGui::Checkbox("Encode (uncheck = Decode)", &jpg_encode);
@@ -154,7 +162,7 @@ inline int run_gui()
         if (ImGui::Button(jpg_encode ? "Encode" : "Decode"))
         {
             jpg_show_msg = false;
-            jpg_done_ok  = false;
+            jpg_done_ok = false;
             if (std::strlen(jpg_in) != 0)
             {
                 if (std::strlen(jpg_out) == 0)
@@ -162,7 +170,8 @@ inline int run_gui()
                     std::string def = change_extension(jpg_in, jpg_encode ? ".jpg" : ".png");
                     std::strncpy(jpg_out, def.c_str(), sizeof(jpg_out));
                 }
-                try {
+                try
+                {
                     if (jpg_encode)
                     {
                         JpgEncoder enc(jpg_in, jpg_out);
@@ -173,7 +182,9 @@ inline int run_gui()
                         JpgDecoder dec(jpg_in, jpg_out);
                         jpg_done_ok = dec.jpeg_decompress();
                     }
-                } catch (...) {
+                }
+                catch (...)
+                {
                     jpg_done_ok = false;
                 }
             }
@@ -181,31 +192,34 @@ inline int run_gui()
         }
 
         if (jpg_show_msg)
-            ImGui::TextColored(ImVec4(0,1,0,1), "Success! Saved to %s", jpg_out);
+            ImGui::TextColored(ImVec4(0, 1, 0, 1), "Success! Saved to %s", jpg_out);
 
         ImGui::End();
 
         // ---------------- PSNR WINDOW ----------------
-        if (first_frame) {
+        if (first_frame)
+        {
             ImGui::SetNextWindowPos(ImVec2(640, 10));
             ImGui::SetNextWindowSize(ImVec2(300, 240));
         }
         ImGui::Begin("PSNR Sweep to CSV");
 
-        ImGui::InputText("Image Path",  psnr_img, IM_ARRAYSIZE(psnr_img));
+        ImGui::InputText("Image Path", psnr_img, IM_ARRAYSIZE(psnr_img));
         ImGui::InputText("CSV Output", psnr_csv, IM_ARRAYSIZE(psnr_csv));
 
         ImGui::Text("Codec");
         ImGui::SameLine();
-        if (ImGui::RadioButton("JPEG", psnr_is_jpeg)) psnr_is_jpeg = true;
+        if (ImGui::RadioButton("JPEG", psnr_is_jpeg))
+            psnr_is_jpeg = true;
         ImGui::SameLine();
-        if (ImGui::RadioButton("HEIC", !psnr_is_jpeg)) psnr_is_jpeg = false;
+        if (ImGui::RadioButton("HEIC", !psnr_is_jpeg))
+            psnr_is_jpeg = false;
         ImGui::Checkbox("Keep temp files", &keep_tmp_files);
 
         if (ImGui::Button("Run Sweep"))
         {
             psnr_show_msg = false;
-            psnr_done_ok  = false;
+            psnr_done_ok = false;
             if (std::strlen(psnr_img) != 0)
             {
                 if (std::strlen(psnr_csv) == 0)
@@ -213,13 +227,16 @@ inline int run_gui()
                     std::string def = change_extension(psnr_img, psnr_is_jpeg ? "_jpeg_psnr.csv" : "_heic_psnr.csv");
                     std::strncpy(psnr_csv, def.c_str(), sizeof(psnr_csv));
                 }
-                try {
+                try
+                {
                     if (psnr_is_jpeg)
                         JpegPSNRtoCSV(psnr_img, psnr_csv, keep_tmp_files);
                     else
                         evaluateHeicQualitySweep(psnr_img, psnr_csv, keep_tmp_files);
                     psnr_done_ok = true;
-                } catch (...) {
+                }
+                catch (...)
+                {
                     psnr_done_ok = false;
                 }
             }
@@ -227,7 +244,7 @@ inline int run_gui()
         }
 
         if (psnr_show_msg)
-            ImGui::TextColored(ImVec4(0,1,0,1), "CSV written to %s", psnr_csv);
+            ImGui::TextColored(ImVec4(0, 1, 0, 1), "CSV written to %s", psnr_csv);
 
         ImGui::End();
 
